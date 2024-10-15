d88447d1a618ce54091ddae79e0b5373809dd99e
Jacopo Luri <jacopo.luri@uqido.com>
2024-10-15 17:44:45 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
-function App() {
-  return <div className="App">aaa</div>;
-}
+const App = () => {
+  const keyPressedref = useRef(false);
+  const [keyState, setKeyState] = useState("");
+
+  const handleKeyPress = (event) => {
+    if (event.key === "4") {
+      !keyPressedref.current ? setKeyState("PRESSED") : setKeyState("HOLDING");
+      keyPressedref.current = true;
+    }
+  };
+
+  const handleKeyRelease = (event) => {
+    if (event.key === "4") {
+      setKeyState("RELEASE");
+      keyPressedref.current = false;
+      setTimeout(() => {
+        setKeyState("");
+      }, 250);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    window.addEventListener("keyup", handleKeyRelease);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+  return <div className="App">KEY 4 IS: {keyState}</div>;
+};
 
 export default App;
